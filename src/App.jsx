0c3c86d62536af87b05a6d56d31876b5e11d1ee7
Startech <startechsky@gmail.com>
2025-04-21 12:04:22 +0100

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>app</div>
+  )
+}
+
+export default app
